03c841310580a1c785861998874018e65b267226
Mike iLL Kilmer <mike@mzoo.org>
2017-08-29 13:32:45 -0500

    Output the nav CSS in critical and pull into HTML.

--- templates/head.php
+++ templates/head.php
@@ -3,4 +3,6 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+  <?php $css_File = file_get_contents(get_stylesheet_directory() . '/dist/styles/critical.min.css'); ?>
+  <style type="text/css"><?php echo $css_File; ?></style>
 </head>
