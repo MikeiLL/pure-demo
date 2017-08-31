cb1d045b444a5ee915bd917bbc535191b4de9906
Mike iLL Kilmer <mike@mzoo.org>
2017-08-31 09:47:24 -0500

    Remove javascript which is now in critical and clean up some other old code.

--- templates/header.php
+++ templates/header.php
@@ -21,28 +21,6 @@ use  Roots\Sage\Extras;
       <!--[if gt IE 8]><!-->
           <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css">
       <!--<![endif]-->
-      <style>
-
-
-      </style>
-
-      <!--<div class="custom-wrapper pure-g" id="menu">
-              <div class="pure-menu pure-u-1 pure-u-md-1-2">
-                  <a href="#" class="pure-menu-heading custom-brand">Brand</a>
-                  <a href="#" class="custom-toggle" id="toggle"><s class="bar"></s><s class="bar"></s></a>
-              </div>
-              <div class=" pure-u-1 pure-u-md-1-2 pure-menu pure-menu-horizontal custom-can-transform">
-                  <ul class="pure-menu-list">
-                      <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
-                      <li class="pure-menu-item"><a href="#" class="pure-menu-link">About</a></li>
-                      <li class="pure-menu-item"><a href="#" class="pure-menu-link">Contact</a></li>
-
-                      <li class="pure-menu-item"><a href="#" class="pure-menu-link">Yahoo</a></li>
-                      <li class="pure-menu-item"><a href="#" class="pure-menu-link">W3C</a></li>
-                  </ul>
-              </div>
-      </div>-->
-
 
     <a href="#" class="custom-toggle" id="toggle"><s class="bar"></s><s class="bar"></s><s class="bar"></s></a>
     <nav class="custom-wrapper overflow-hide navigation-container pure-menu custom-restricted-width pure-u-1" id="menu">
@@ -56,51 +34,6 @@ use  Roots\Sage\Extras;
     endif; ?>
     </nav>
 
-      <script>
-      (function (window, document) {
-      var menu = document.getElementById('menu'),
-          WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
-
-      function toggleHorizontal() {
-          var menu = document.getElementById('menu');
-          menu.classList.toggle('overflow-hide');
-          menu.classList.toggle('overflow-visible');
-          [].forEach.call(
-              menu.querySelectorAll('.custom-can-transform'),
-              function(el){
-                  el.classList.toggle('pure-menu-horizontal');
-              }
-          );
-      };
-
-      function toggleMenu() {
-          // set timeout so that the panel has a chance to roll up
-          // before the menu switches states
-          if (menu.classList.contains('open')) {
-              setTimeout(toggleHorizontal, 500);
-          }
-          else {
-              toggleHorizontal();
-          }
-          menu.classList.toggle('open');
-          document.getElementById('toggle').classList.toggle('x');
-      };
-
-      function closeMenu() {
-          if (menu.classList.contains('open')) {
-              toggleMenu();
-          }
-      }
-
-      document.getElementById('toggle').addEventListener('click', function (e) {
-          toggleMenu();
-          e.preventDefault();
-      });
-
-      window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
-      })(this, this.document);
-
-      </script>
   </header>
 </div><?php if (HeroRender\puredemo_has_hero()) HeroRender\puredemo_get_her(); ?>
 
